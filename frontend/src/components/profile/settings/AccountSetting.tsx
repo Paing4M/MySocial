2d48c2c740e95ab687ad7c84d738a7fcf0e6db2e
Paing4M <paing.2000@yahoo.com>
2024-04-22 16:37:10 +0630

    profile_settings_account

--- frontend/src/components/profile/settings/AccountSetting.tsx
+++ frontend/src/components/profile/settings/AccountSetting.tsx
@@ -1,5 +1,22 @@
+import { Button } from '@/components/ui/button'
+
 const AccountSetting = () => {
-	return <div>account</div>
+	return (
+		<div>
+			<h4 className='mb-4 text-lg'>Delete account </h4>
+
+			<p className='text-sm text-muted-foreground'>
+				This action is irreversible and will permanently delete all your
+				data associated with the account.
+			</p>
+
+			<div className='mt-6'>
+				<Button variant={'outline'} className='border-red-600 text-red-500'>
+					Delete My Account
+				</Button>
+			</div>
+		</div>
+	)
 }
 
 export default AccountSetting
